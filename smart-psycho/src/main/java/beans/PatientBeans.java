f59ffb1efe02b7315978de32230f26958a6102aa
Samih KADRI <samih.kadri@gmail.com>
2015-12-21 19:18:28 +0100

    added Stateless to patient service
    Signed-off-by: Samih KADRI <samih.kadri@gmail.com>

--- smart-psycho/src/main/java/beans/PatientBeans.java
+++ smart-psycho/src/main/java/beans/PatientBeans.java
@@ -1,5 +1,7 @@
 package beans;
 
+import java.util.Date;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
@@ -11,6 +13,61 @@ public class PatientBeans {
 
 	
 	private Patient patient;
+	
+	private String fullName;
+	private Date dateNaissance;
+	private String description;
+	private String login;
+	private String password;
+	private Date dateInscription;
+	
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getDateInscription() {
+		return dateInscription;
+	}
+
+	public void setDateInscription(Date dateInscription) {
+		this.dateInscription = dateInscription;
+	}
 
 	@EJB PatientServicesLocal patientServicesLocal;
 	
